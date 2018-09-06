17c45d0baba37a72a45f0b97eac7788059d999b6
Michal Vala <michal.vala@gmail.com>
2018-09-06 06:55:24 +0200

    project cards - create deck with few cards and print

---
+++ project_cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	cards := deck{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+	cards.print()
+}
+
+func newCard() string {
+	return "Ace of Diamonds"
+}
