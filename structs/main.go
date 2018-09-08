619c98ca5b79d6eef4801c6f1e4b4134572d8b86
Michal Vala <michal.vala@gmail.com>
2018-09-08 08:29:30 +0200

    embedded structs

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,22 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zip   int
 }
 
 func main() {
-	john := person{"John", "Doe"}
+	john := person{"John", "Doe", contactInfo{"john@doe", 123456}}
 	fmt.Println(john)
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact:   contactInfo{email: "alex@anderson", zip: 123456}}
 	fmt.Println(alex)
 
 	var jane person
@@ -19,5 +28,7 @@ func main() {
 	fmt.Printf("%+v\n", jane)
 	jane.firstName = "Jane"
 	jane.lastName = "Doe"
+	jane.contact.email = "jane@doe"
+	jane.contact.zip = 123456
 	fmt.Printf("%+v\n", jane)
 }
