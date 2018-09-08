a2e54d4c580d43db5582575023d81d20bf827370
Michal Vala <michal.vala@gmail.com>
2018-09-08 07:32:50 +0200

    assignment 1 - loop int slice, print even/odd

---
+++ assignment1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers []int
+	for i := 0; i < 10; i++ {
+		numbers = append(numbers, i)
+	}
+
+	for no := range numbers {
+		if no % 2 == 0 {
+			fmt.Println(no, " is even")
+		} else {
+			fmt.Println(no, "is odd")
+		}
+	}
+}
