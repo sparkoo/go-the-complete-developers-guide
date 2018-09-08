88cc29270457fecf4b086bf2b137a898469c9c9f
Michal Vala <michal.vala@gmail.com>
2018-09-08 06:59:43 +0200

    test new deck's first and last card

--- project_cards/deck_test.go
+++ project_cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected size of deck to be 52, but got [%v]", len(d))
 	}
+
+	if d[0] != "Ace of Diamonds" {
+		t.Errorf("Expected first card to be Ace of Diamonds, but got [%v]", d[0])
+	}
+
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but got [%v]", d[len(d) - 1])
+	}
 }
