673e2afa93f46f583417aad42b648b39e541421c
Michal Vala <michal.vala@gmail.com>
2018-09-09 07:33:09 +0200

    map loop

--- maps/main.go
+++ maps/main.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	colors := map[string]string{
-		"red": "#ff0000",
+		"red":   "#ff0000",
 		"green": "#00ff00",
+		"white": "#ffffff",
 	}
 	fmt.Println(colors)
 
@@ -20,4 +21,12 @@ func main() {
 
 	delete(colors3, "white")
 	fmt.Println(colors3)
+
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for k, v := range m {
+		fmt.Printf("%v => %v\n", k, v)
+	}
 }
