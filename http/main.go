d6987b877597116fbbf5726f24786a75a82ba73d
Michal Vala <michal.vala@gmail.com>
2018-09-09 14:09:19 +0200

    print http response

--- http/main.go
+++ http/main.go
@@ -7,11 +7,24 @@ import (
 )
 
 func main() {
-	res, err := http.Get("http://google.com")
+	res, err := http.Get("http://seznam.cz")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(res)
+	b := make([]byte, 1024)
+	var content []byte
+
+	for i, err := res.Body.Read(b); i > 0 && err == nil; i, err = res.Body.Read(b) {
+		if err != nil {
+			fmt.Println("Error: ", err)
+			res.Body.Close()
+			os.Exit(1)
+		}
+
+		content = append(content, b[:i]...)
+	}
+	fmt.Println(string(content))
+	res.Body.Close()
 }
