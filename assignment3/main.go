30a60d4b0a27129927ef01a02a9957be3b20813c
Michal Vala <michal.vala@gmail.com>
2018-09-10 07:17:21 +0200

    assignment 3 - print file from arg to stdout

---
+++ assignment3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	filename := os.Args[1]
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, f)
+}
