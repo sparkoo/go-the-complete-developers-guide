dc76777e0e8aa7b9b162821285b2ce304fd47f65
Michal Vala <michal.vala@gmail.com>
2018-09-10 17:36:22 +0200

    ping links in infinite for

--- channels/main.go
+++ channels/main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	links := []string{
-		"http://google.com",
+		//"http://google.com",
 		"http://facebook.com",
-		"http://seznam.cz",
+		//"http://seznam.cz",
 		"http://golang.com",
-		"http://stackoverflow.com",
-		"http://sparko.cz",
-		"http://blabol.czf",
+		//"http://stackoverflow.com",
+		//"http://sparko.cz",
+		//"http://blabol.czf",
 	}
 
 	c := make(chan string)
@@ -22,18 +22,18 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
 func checkLink(link string, c chan string) {
-	res, err := http.Get(link)
+	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is off!")
-		c <- err.Error()
+		c <- link
 		return
 	}
 	fmt.Println(link, "is on")
-	c <- res.Status
+	c <- link
 }
