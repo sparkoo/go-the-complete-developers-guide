c35ece9d1380738f9c01c38282535fb12994aef8
Michal Vala <mvala@redhat.com>
2018-09-10 09:31:45 +0200

    check links if for loop

---
+++ channels/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://seznam.cz",
+		"http://golang.com",
+		"http://stackoverflow.com",
+		"http://sparko.cz",
+		"http://blabol.czf",
+	}
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string)  {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is off!")
+		return
+	}
+
+	fmt.Println(link, "is on!")
+}
